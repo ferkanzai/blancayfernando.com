f0acc0ed007dba307c7aad7dd20d5b0f7065966f
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2024-03-24 03:55:38 +0100

    fix: title styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default async function Home() {
   return (
     <main className="flex min-h-full flex-col items-center justify-center gap-2 px-4 text-center md:px-10">
       <ImageCarousel />
-      <h1 className="font-maginia text-4xl font-bold text-[#93987C]">
-        19 de Octubre de 2024
+      <h1 className="py-8 font-maginia text-4xl font-bold text-[#93987C]">
+        19 Octubre 2024
       </h1>
       <div className="h-80 w-full sm:w-3/5">
         <MapWrapper />
