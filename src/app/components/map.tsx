df25db72812d1f447e23e80da00dc5a512fb8bf8
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2024-03-24 03:47:40 +0100

    feat: map information

---
+++ src/app/components/map.tsx
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+"use client";
+
+import { Status, Wrapper } from "@googlemaps/react-wrapper";
+import {
+  Children,
+  cloneElement,
+  isValidElement,
+  useEffect,
+  useRef,
+  useState,
+  type ReactNode,
+} from "react";
+
+const render = (status: Status) => {
+  if (status === Status.LOADING) {
+    return <h1>Loading...</h1>;
+  }
+  return <h1>{status}</h1>;
+};
+
+export function MapWrapper() {
+  return (
+    <Wrapper apiKey={"AIzaSyBokc_FkZk93BkUYzjekwFj3r_W9QRMP_E"} render={render}>
+      <Map>
+        <Marker
+          position={{ lat: 40.40548, lng: -3.68363 }}
+          title="Real Basílica Nuestra Señora de Atocha"
+          label="1"
+        />
+        <Marker
+          position={{ lat: 40.40535, lng: -3.6829 }}
+          title="Fábrica de Tapices"
+          label="2"
+        />
+      </Map>
+    </Wrapper>
+  );
+}
+
+export function Map({ children }: { children: ReactNode }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [map, setMap] = useState<google.maps.Map>();
+
+  useEffect(() => {
+    if (ref.current && !map) {
+      setMap(
+        new window.google.maps.Map(ref.current, {
+          zoom: 17,
+          center: { lat: 40.4055, lng: -3.6835 },
+          fullscreenControl: false,
+          scaleControl: false,
+          streetViewControl: false,
+          mapTypeControl: false,
+          disableDefaultUI: true,
+          zoomControl: false,
+          disableDoubleClickZoom: true,
+          draggable: false,
+          clickableIcons: false,
+        }),
+      );
+    }
+  }, [ref, map]);
+
+  return (
+    <>
+      <div ref={ref} id="map" className="flex h-full" />
+      {Children.map(children, (child) => {
+        if (isValidElement(child)) {
+          // @ts-expect-error map prop is required
+          return cloneElement(child, { map });
+        }
+      })}
+    </>
+  );
+}
+
+const Marker = (options: google.maps.MarkerOptions) => {
+  const [marker, setMarker] = useState<google.maps.Marker>();
+
+  useEffect(() => {
+    if (!marker) {
+      setMarker(
+        new google.maps.Marker({
+          ...options,
+        }),
+      );
+    }
+
+    // remove marker from map on unmount
+    return () => {
+      if (marker) {
+        marker.setMap(null);
+      }
+    };
+  }, [marker]);
+
+  useEffect(() => {
+    if (marker) {
+      marker.setOptions(options);
+    }
+  }, [marker, options]);
+
+  return null;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { ImageCarousel } from "./components/image-carousel";
+import { MapWrapper } from "./components/map";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-full flex-col items-center justify-center px-4 text-center md:px-10">
+    <main className="flex min-h-full flex-col items-center justify-center gap-2 px-4 text-center md:px-10">
       <ImageCarousel />
+      <h1 className="font-maginia text-4xl font-bold text-[#93987C]">
+        19 de Octubre de 2024
+      </h1>
+      <div className="h-80 w-3/5">
+        <MapWrapper />
+      </div>
     </main>
   );
 }
