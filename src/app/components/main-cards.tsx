641b87cebb4df93af4a3fdeca60fbc076a128aa9
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2024-03-26 21:59:06 +0100

    feat: rsvp main card

---
+++ src/app/components/main-cards.tsx
@@ -0,0 +1,31 @@
+import { Gem, PartyPopper } from "lucide-react";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
+import Link from "next/link";
+
+export default function MainCards() {
+  return (
+    <div className="pb-6">
+      <Link href="/rsvp">
+        <Card className="group w-full max-w-[450px] cursor-pointer bg-transparent hover:-translate-y-2 hover:shadow-md hover:shadow-accent">
+          <CardHeader>
+            <CardTitle className="flex items-center justify-between gap-2">
+              <PartyPopper className="h-12 w-12 sm:h-8 sm:w-8 sm:group-hover:animate-bounce sm:group-hover:stroke-accent sm:group-hover:transition-all" />
+              ¡Confirma tu asistencia!
+              <Gem className="h-12 w-12 sm:h-8 sm:w-8 sm:group-hover:animate-bounce sm:group-hover:stroke-accent sm:group-hover:transition-all sm:group-hover:delay-75" />
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            Ayúdanos a planificar mejor el evento confirmando tu asistencia. ¡Te
+            esperamos!
+          </CardContent>
+        </Card>
+      </Link>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import { ImageCarousel } from "./components/image-carousel";
-import { MapWrapper } from "./components/map";
+import { ImageCarousel } from "@/app/components/image-carousel";
+import { MapWrapper } from "@/app/components/map";
+import MainCards from "@/app/components/main-cards";
 
 export default async function Home() {
   return (
@@ -8,6 +9,7 @@ export default async function Home() {
       <h1 className="py-8 font-maginia text-4xl font-bold text-[#93987C]">
         19 Octubre 2024
       </h1>
+      <MainCards />
       <MapWrapper />
     </main>
   );
