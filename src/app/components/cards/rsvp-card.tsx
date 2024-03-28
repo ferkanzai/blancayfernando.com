1e42d320912587e9e15cfbde1d714d928573f591
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2024-03-28 18:10:47 +0100

    fix: remove icons bouncing hover animation

--- src/app/components/cards/rsvp-card.tsx
+++ src/app/components/cards/rsvp-card.tsx
@@ -14,9 +14,11 @@ export default function RsvpCard() {
       <Card className="group w-full max-w-[450px] cursor-pointer bg-transparent hover:-translate-y-2 hover:shadow-md hover:shadow-accent">
         <CardHeader>
           <CardTitle className="flex items-center justify-between gap-2">
-            <PartyPopper className="h-12 w-12 sm:h-8 sm:w-8 sm:group-hover:animate-bounce sm:group-hover:stroke-accent sm:group-hover:transition-all" />
-            ¡Confirma tu asistencia!
-            <Gem className="h-12 w-12 sm:h-8 sm:w-8 sm:group-hover:animate-bounce sm:group-hover:stroke-accent sm:group-hover:transition-all sm:group-hover:delay-75" />
+            <PartyPopper className="h-12 w-12 sm:h-8 sm:w-8 sm:group-hover:stroke-secondary" />
+            <span className="sm:group-hover:text-secondary">
+              ¡Confirma tu asistencia!
+            </span>
+            <Gem className="h-12 w-12 sm:h-8 sm:w-8 sm:group-hover:stroke-secondary" />
           </CardTitle>
         </CardHeader>
         <CardContent>
