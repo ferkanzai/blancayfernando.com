497f0b2672aac83d96b9920e86970fd02a124199
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2024-07-01 21:31:15 +0200

    feat: initial form table

--- src/app/components/admin-table/columns.tsx
+++ src/app/components/admin-table/columns.tsx
@@ -1,15 +1,39 @@
 "use client";
 
+import { CaretSortIcon } from "@radix-ui/react-icons";
 import { createColumnHelper, type ColumnDef } from "@tanstack/react-table";
 
+import { Button } from "@/app/components/ui/button";
+import { Checkbox } from "@/app/components/ui/checkbox";
 import { type FormularySelect } from "@/server/db/schema";
-import { Button } from "../ui/button";
-import { ArrowDownUpIcon } from "lucide-react";
 
 const columnHelper = createColumnHelper<FormularySelect>();
 
-export const columns: ColumnDef<FormularySelect> = [
-  columnHelper.accessor("name", {
+export const columns: ColumnDef<FormularySelect>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value: boolean) =>
+          table.toggleAllPageRowsSelected(!!value)
+        }
+        aria-label="Seleccionar todas las filas"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value: boolean) => row.toggleSelected(!!value)}
+        aria-label="Seleccionar fila"
+      />
+    ),
+  },
+  {
+    accessorKey: "name",
     header: ({ column }) => {
       return (
         <Button
@@ -17,10 +41,42 @@ export const columns: ColumnDef<FormularySelect> = [
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
           Nombre y apellidos
-          <ArrowDownUpIcon className="ml-2 h-4 w-4" />
+          <CaretSortIcon className="ml-2 h-4 w-4" />
         </Button>
       );
     },
     minSize: 200,
-  }),
+  },
+  {
+    accessorKey: "coming",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Asiste
+          <CaretSortIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (row.getValue("coming") ? "✅" : "❌"),
+    meta: {
+      align: "center",
+    },
+  },
+  {
+    accessorKey: "allergies",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Allergies
+          <CaretSortIcon className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
 ];
--- src/app/components/admin.tsx
+++ src/app/components/admin.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { columns } from "@/app/components/admin-table/columns";
+import { DataTable } from "@/app/components/admin-table/data-table";
+import Stats from "@/app/components/stats";
 import { api } from "@/trpc/react";
-import Stats from "./stats";
 
 export default function Admin() {
   const { data, isFetching, isLoading, isRefetching } =
@@ -22,16 +24,7 @@ export default function Admin() {
     <>
       <Stats stats={stats} isLoading={loading} />
       <h2 className="text-center text-3xl font-bold">📗 Lista completa</h2>
-      {data?.map((row) => {
-        return (
-          <div className="flex gap-4" key={row.id}>
-            <p>{row.id}</p>
-            <p>{row.name}</p>
-            <p>{row.associatedTo}</p>
-            <p>{row.allergies}</p>
-          </div>
-        );
-      })}
+      <DataTable columns={columns} data={data} isLoading={loading} />
     </>
   );
 }
