dcc125881f7f9ebe151fff1adaea4f7dd5eccbb6
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2024-07-07 00:27:12 +0200

    fix: iframe width

--- src/app/musica/page.tsx
+++ src/app/musica/page.tsx
@@ -4,8 +4,6 @@ import { getServerAuthSession } from "@/server/auth";
 export default async function musicPage() {
   const session = await getServerAuthSession();
 
-  console.log(session);
-
   return (
     <>
       <h1 className="font-maginia text-3xl font-bold text-primary">
@@ -14,16 +12,18 @@ export default async function musicPage() {
 
       <SearchSpotify session={session} />
 
-      <iframe
-        style={{ borderRadius: "12px" }}
-        src="https://open.spotify.com/embed/playlist/7gAoZBejGkkvdz14ossFBr"
-        width="100%"
-        height="352"
-        frameBorder="0"
-        allowFullScreen
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
+      <div className="w-full md:max-w-2xl">
+        <iframe
+          style={{ borderRadius: "12px" }}
+          src="https://open.spotify.com/embed/playlist/7gAoZBejGkkvdz14ossFBr"
+          width="100%"
+          height="352"
+          frameBorder="0"
+          allowFullScreen
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
+      </div>
     </>
   );
 }
