840745620cbb2b708808a8acee2dd82be355d097
Fernando Carmona Ayuela <software.development.15@olympicchannel.com>
2024-07-29 10:41:22 +0200

    feat: add gift card to thanks page

--- src/app/informacion/page.tsx
+++ src/app/informacion/page.tsx
@@ -2,8 +2,8 @@ import InfoData from "@/app/components/info-data";
 
 export default function FAQPage() {
   return (
-    <>
+    <div className="flex w-full flex-col items-center justify-center pb-16">
       <InfoData />
-    </>
+    </div>
   );
 }
