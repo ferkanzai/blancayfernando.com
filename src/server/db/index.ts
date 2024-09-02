5db3fffef28715269b344c3fbb1bf506a9c4a9b2
fernando-tpbs <fernando.carmona@thepowermba.com>
2024-09-02 09:54:38 +0200

    fix: neon drizzle connection por la cara

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,4 +1,4 @@
-import { neon } from "@neondatabase/serverless";
+import { neon, type NeonQueryFunction } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
 import { env } from "@/env";
@@ -6,4 +6,7 @@ import * as schema from "./schema";
 
 const sql = neon(env.DATABASE_URL);
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(
+  sql as unknown as NeonQueryFunction<boolean, boolean>,
+  { schema },
+);
