24fb9f471f016f8fb2e95172a2e810276006d064
ferkanzai <fernando.carayu@gmail.com>
2024-09-02 10:45:38 +0200

    fix: deprecated framerBorder

--- src/app/musica/page.tsx
+++ src/app/musica/page.tsx
@@ -15,11 +15,10 @@ export default async function musicPage() {
 
       <div className="w-full pb-10 md:max-w-2xl">
         <iframe
-          style={{ borderRadius: "12px" }}
+          style={{ borderRadius: "12px", border: "none" }}
           src={`https://open.spotify.com/embed/playlist/${env.SPOTIFY_PLAYLIST_ID}`}
           width="100%"
           height="352"
-          frameBorder="0"
           allowFullScreen
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
